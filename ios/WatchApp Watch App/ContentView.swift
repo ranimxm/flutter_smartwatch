c556ee2e0e3706a421ca6eede23bb63f270501ee
ranim <ranimm01@hotmail.com>
2024-12-15 22:39:15 +0100

    adding first statis page

--- ios/WatchApp Watch App/ContentView.swift
+++ ios/WatchApp Watch App/ContentView.swift
@@ -9,13 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            HStack(spacing: 20) {
+                // snelheid
+                VStack(spacing: 5) {
+                    Image(systemName: "speedometer")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                        .foregroundColor(AppColors.yellow)
+                    Text("20")
+                        .font(.system(size: 50))
+                        .foregroundColor(AppColors.yellow)
+                    Text("km/h")
+                        .font(.system(size: 20))
+                        .foregroundColor(AppColors.yellow80)
+                }
+                // hartslag
+                VStack(spacing: 5) {
+                    Image(systemName: "heart.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                        .foregroundColor(AppColors.red)
+                    Text("80")
+                        .font(.system(size: 50))
+                        .foregroundColor(AppColors.red)
+                    Text("bpm")
+                        .font(.system(size: 20))
+                        .foregroundColor(AppColors.red80)
+                }
+            }
+            // timer
+            Text("08")
+                .font(.system(size: 30))
+                .foregroundColor(AppColors.lightBlue)
+            + Text(" min ")
+                .font(.system(size: 20))
+                .foregroundColor(AppColors.lightBlue)
+            + Text("22")
+                .font(.system(size: 30))
+                .foregroundColor(AppColors.lightBlue)
+            + Text(" sec")
+                .font(.system(size: 20))
+                .foregroundColor(AppColors.lightBlue)
+
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(AppColors.darkBlue)
     }
 }
 
